426d09c6240144fa38ba77e8b4065f948ce11378
Dominic Barnes <dominic@segment.io>
2015-01-24 20:22:34 -0700

    versioning the tests

--- test/server.js
+++ test/server.js
@@ -1,5 +1,4 @@
 var assert = require('assert');
-var supertest = require('supertest');
 var mock = require('..');
 
 describe('server', function () {
@@ -11,32 +10,5 @@ describe('server', function () {
   it('should use a specified url', function () {
     var srv = mock.server({ url: 'http://myapp.iriscouch.com/' });
     assert.equal(srv.url(), 'http://myapp.iriscouch.com/');
-  });
-
-  describe('server.info()', function () {
-    var server = mock.server();
-    var request = supertest(server.url());
-
-    afterEach(function () {
-      server.mock.done();
-    });
-
-    it('should mock the root endpoint', function (done) {
-      server.info();
-      var version = server.options.version;
-
-      request
-        .get('')
-        .expect(200)
-        .expect('Cache-Control', 'must-revalidate')
-        .expect('Content-Length', /^\d+$/)
-        .expect('Date', server.options.date.toUTCString())
-        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
-        .expect(function (res) {
-          assert.equal(res.body.couchdb, 'Welcome');
-          assert.equal(res.body.version, version);
-        })
-        .end(done);
-    });
   });
 });
---
+++ test/versions/1.6.1/server.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var supertest = require('supertest');
+var mock = require('../../..');
+
+describe('1.6.1', function () {
+  describe('server.info()', function () {
+    var server = mock.server();
+    var request = supertest(server.url());
+
+    afterEach(function () {
+      server.mock.done();
+    });
+
+    it('should mock the root endpoint', function (done) {
+      server.info();
+      var version = server.options.version;
+
+      request
+        .get('')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Length', /^\d+$/)
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          assert.equal(res.body.couchdb, 'Welcome');
+          assert.equal(res.body.version, version);
+        })
+        .end(done);
+    });
+  });
+});

ead2c5988e4e13dc093188f9af5221b2085c1096
Dominic Barnes <dominic@segment.io>
2015-01-26 09:15:45 -0700

    adding _all_dbs endpoint, upgrading nock and removing some extra headers

--- test/1.6.1/server.js
+++ test/1.6.1/server.js
@@ -62,4 +62,50 @@ module.exports = function () {
         .end(done);
     });
   });
+
+  describe('.allDbs([count])', function () {
+    it('should mock the all dbs endpoint', function (done) {
+      server.allDbs();
+
+      request
+        .get('_all_dbs')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Length', /^\d+$/)
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          var data = res.body;
+          assert(Array.isArray(data));
+          assert(data.length >= 1 && data.length <= 10);
+          assert.equal(data[0], '_users');
+          assert.equal(data[1], '_replicator');
+        })
+        .end(done);
+    });
+
+    it('should allow setting a custom number of non-system dbs', function (done) {
+      server.allDbs(13);
+
+      request
+        .get('_all_dbs')
+        .expect(200)
+        .expect(function (res) {
+          assert.equal(res.body.length, 15);
+        })
+        .end(done);
+    });
+
+    it('should always include the system dbs', function (done) {
+      server.allDbs(0);
+
+      request
+        .get('_all_dbs')
+        .expect(200)
+        .expect(function (res) {
+          assert.deepEqual(res.body, [ '_users', '_replicator' ]);
+        })
+        .end(done);
+    });
+  });
 };

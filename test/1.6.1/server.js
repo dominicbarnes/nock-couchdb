0b66b3e77aaf7a2beef5950d9fb3da46c5235854
Dominic Barnes <dominic@segment.io>
2015-01-25 22:22:18 -0700

    adding _active_tasks endpoint

--- test/1.6.1/server.js
+++ test/1.6.1/server.js
@@ -3,17 +3,17 @@ var supertest = require('supertest');
 var mock = require('../..');
 
 module.exports = function () {
+  var server = mock.server();
+  var request = supertest(server.url());
+  var version = server.options.version;
+
+  afterEach(function () {
+    server.mock.done();
+  });
+
   describe('.info()', function () {
-    var server = mock.server();
-    var request = supertest(server.url());
-
-    afterEach(function () {
-      server.mock.done();
-    });
-
     it('should mock the root endpoint', function (done) {
       server.info();
-      var version = server.options.version;
 
       request
         .get('')
@@ -25,6 +25,39 @@ module.exports = function () {
         .expect(function (res) {
           assert.equal(res.body.couchdb, 'Welcome');
           assert.equal(res.body.version, version);
+        })
+        .end(done);
+    });
+  });
+
+  describe('.activeTasks([count])', function () {
+    it('should mock the active tasks endpoint', function (done) {
+      server.activeTasks();
+
+      request
+        .get('_active_tasks')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Length', /^\d+$/)
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          var data = res.body;
+          assert(Array.isArray(data));
+          assert(data.length >= 1 && data.length <= 10);
+          assert(data[0].type);
+        })
+        .end(done);
+    });
+
+    it('should allow setting a custom number of tasks', function (done) {
+      server.activeTasks(15);
+
+      request
+        .get('_active_tasks')
+        .expect(200)
+        .expect(function (res) {
+          assert.equal(res.body.length, 15);
         })
         .end(done);
     });

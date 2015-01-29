52e2f8e92e39af8b8c387dade96ffad3037e1383
Dominic Barnes <dominic@segment.io>
2015-01-28 22:13:18 -0700

    adding log, restart and stats endpoints

--- test/1.6.1/server.js
+++ test/1.6.1/server.js
@@ -20,6 +20,7 @@ module.exports = function () {
         .expect(200)
         .expect('Cache-Control', 'must-revalidate')
         .expect('Content-Length', /^\d+$/)
+        .expect('Content-Type', 'application/json')
         .expect('Date', server.options.date.toUTCString())
         .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
         .expect(function (res) {
@@ -39,6 +40,7 @@ module.exports = function () {
         .expect(200)
         .expect('Cache-Control', 'must-revalidate')
         .expect('Content-Length', /^\d+$/)
+        .expect('Content-Type', 'application/json')
         .expect('Date', server.options.date.toUTCString())
         .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
         .expect(function (res) {
@@ -72,6 +74,7 @@ module.exports = function () {
         .expect(200)
         .expect('Cache-Control', 'must-revalidate')
         .expect('Content-Length', /^\d+$/)
+        .expect('Content-Type', 'application/json')
         .expect('Date', server.options.date.toUTCString())
         .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
         .expect(function (res) {
@@ -105,6 +108,195 @@ module.exports = function () {
         .expect(function (res) {
           assert.deepEqual(res.body, [ '_users', '_replicator' ]);
         })
+        .end(done);
+    });
+  });
+
+  describe('.log([query])', function () {
+    it('should mock the log endpoint', function (done) {
+      server.log();
+
+      request
+        .get('_log')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'text/plain')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Transfer-Encoding', 'chunked')
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          var data = res.text;
+          assert(data);
+          assert.equal(data.length, 1000);
+        })
+        .end(done);
+    });
+  });
+
+  describe('.restart([options])', function () {
+    it('should mock a successful request', function (done) {
+      server.restart();
+
+      request
+        .post('_restart')
+        .expect(202, { ok: true })
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .end(done);
+    });
+
+    it('should mock an authentication failure', function (done) {
+      server.restart({ error: 403 });
+
+      request
+        .post('_restart')
+        .expect(403, {
+          error:  'unauthorized',
+          reason: 'You are not a server admin.'
+        })
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .end(done);
+    });
+
+    it('should mock a content-type failure', function (done) {
+      server.restart({ error: 415 });
+
+      request
+        .post('_restart')
+        .expect(415, {
+          error:  'bad_content_type',
+          reason: 'Content-Type must be application/json'
+        })
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .end(done);
+    });
+  });
+
+  describe('.stats([options])', function () {
+    it('should mock a successful request', function (done) {
+      server.stats();
+
+      request
+        .get('_stats')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          var data = res.body;
+          Object.keys(data).forEach(function (group) {
+            data[group] = Object.keys(data[group]);
+          });
+          assert.deepEqual(data, {
+            couchdb: [
+              'auth_cache_hits',
+              'auth_cache_misses',
+              'database_reads',
+              'database_writes',
+              'open_databases',
+              'open_os_files',
+              'request_time'
+            ],
+            httpd_request_methods: [
+              'COPY', 'DELETE', 'GET',
+              'HEAD', 'POST', 'PUT'
+            ],
+            httpd_status_codes: [
+              200, 201, 202,
+              301, 304,
+              400, 401, 403, 404, 405, 409, 412,
+              500
+            ],
+            httpd: [
+              'bulk_requests',
+              'clients_requesting_changes',
+              'requests',
+              'temporary_view_reads',
+              'view_reads'
+            ]
+          });
+        })
+        .end(done);
+    });
+
+    it('should mock a successful request for only a subset', function (done) {
+      server.stats({
+        group: 'httpd_request_methods',
+        key:   'GET'
+      });
+
+      request
+        .get('_stats/httpd_request_methods/GET')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          var data = res.body;
+          assert.deepEqual(Object.keys(data), [ 'httpd_request_methods' ]);
+          assert.deepEqual(Object.keys(data.httpd_request_methods), [ 'GET' ]);
+        })
+        .end(done);
+    });
+
+    it('should mock a successful request for a non-existant group/key', function (done) {
+      server.stats({
+        group: 'test',
+        key:   'test',
+        empty: true
+      });
+
+      request
+        .get('_stats/test/test')
+        .expect(200)
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+        .expect(function (res) {
+          assert.deepEqual(res.body, {
+            test: {
+              test: {
+                current:     null,
+                description: '',
+                max:         null,
+                mean:        null,
+                min:         null,
+                stddev:      null,
+                sum:         null
+              }
+            }
+          });
+        })
+        .end(done);
+    });
+
+    it('should mock a failed request for a group only (missing key)', function (done) {
+      server.stats({
+        group: 'test',
+        error: 400
+      });
+
+      request
+        .get('_stats/test')
+        .expect(400, {
+          error:  'bad_request',
+          reason: 'Stat names must have exactly two parts.'
+        })
+        .expect('Cache-Control', 'must-revalidate')
+        .expect('Content-Type', 'application/json')
+        .expect('Date', server.options.date.toUTCString())
+        .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
         .end(done);
     });
   });

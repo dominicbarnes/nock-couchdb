2446bebdd6f8ec5a7af3f790a5025a67da570ca5
Dominic Barnes <dominic@segment.io>
2015-02-07 17:48:42 -0800

    adding _db_updates endpoint, simplifying log

---
+++ test/1.6.1/server/db-changes.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var supertest = require('supertest');
+var mock = require('../../..');
+
+module.exports = function () {
+  var server = mock.server();
+  var request = supertest(server.url());
+  var version = server.options.version;
+
+  afterEach(function () {
+    server.mock.done();
+  });
+
+  it('should mock a successful longpoll request', function (done) {
+    server.dbChanges();
+    var start = new Date();
+
+    this.slow(1000);
+
+    request
+      .get('_db_changes')
+      .expect(200)
+      .expect('Cache-Control', 'must-revalidate')
+      .expect('Content-Type', 'application/json')
+      .expect('Date', server.options.date.toUTCString())
+      .expect('Transfer-Encoding', 'chunked')
+      .expect('Server', `CouchDB/${version} (Erlang/OTP)`)
+      .expect(function (res) {
+        var data = res.body;
+        var end = new Date();
+        assert.equal(typeof data.db_name, 'string');
+        assert.strictEqual(data.ok, true);
+        assert.equal(typeof data.type, 'string');
+        assert((end - start) >= 250);
+      })
+      .end(done);
+  });
+};
--- test/1.6.1/server/index.js
+++ test/1.6.1/server/index.js
@@ -2,6 +2,7 @@ module.exports = function () {
   describe('.info()', require('./info'));
   describe('.activeTasks([options])', require('./active-tasks'));
   describe('.allDbs([options])', require('./all-dbs'));
+  describe('.dbChanges([options])', require('./db-changes'));
   describe('.log([options])', require('./log'));
   describe('.restart([options])', require('./restart'));
   describe('.stats([options])', require('./stats'));
